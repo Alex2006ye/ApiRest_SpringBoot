9b92c12e1c89245208592af74fa1307d947c7f3a
Alex2006ye <alexcarvalhoaraujobastos@gmail.com>
2026-01-22 10:26:08 -0300

    feat: acrescentando o swagger no Usuario Controller

--- src/main/java/com/example/FazendoAPI/controller/UsuarioController.java
+++ src/main/java/com/example/FazendoAPI/controller/UsuarioController.java
@@ -4,6 +4,10 @@ import com.example.FazendoAPI.dto.UsuarioCreateDTO;
 import com.example.FazendoAPI.dto.UsuarioUpdateDTO;
 import com.example.FazendoAPI.model.Usuario;
 import com.example.FazendoAPI.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +15,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+//serve para dar um nome para a API e a descrição do que ela faz
+@Tag(name = "Usuario API", description = "Gerenciamento de Usuários")
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
@@ -20,6 +26,13 @@ public class UsuarioController {
         this.service = service;
     }
 
+    //Serve para descrever a operação realizada pela requisição
+    @Operation(summary = "Criar um usuário", description = "Faz a criação de usuários")
+    //aqui é para indicar, o que deve ser respondido, dependendo da resposta
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuário Criado"),
+            @ApiResponse(responseCode = "409", description = "Usuário já existe")
+    })
     @PostMapping("/criar")
     public ResponseEntity<UsuarioCreateDTO> criar(@RequestBody UsuarioCreateDTO dto){ //Não se esquecer dessa annotation
         Usuario usuario = new Usuario();
@@ -34,16 +47,31 @@ public class UsuarioController {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
     }
 
+    @Operation(summary = "Listar usuários", description = "Faz a listagem de todos os usuários cadastrados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário encontrados"),
+            @ApiResponse(responseCode = "404", description = "Usuários não encontrados")
+    })
     @GetMapping("/listar")
     public List<Usuario> listar(){
         return service.listar();
     }
 
+    @Operation(summary = "Exclusão de usuários", description = "Faz a exclusão de um usuário pelo seu id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário deletado"),
+            @ApiResponse(responseCode = "404", description = "Usuários não encontrado")
+    })
     @DeleteMapping("/excluir/{id}")
     public void deletar(@PathVariable Integer id){
         service.deletar(id);
     }
 
+    @Operation(summary = "Modificação de usuários", description = "Faz a alteração de dados de um usuário pelo seu id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário alterado"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+    })
     @PutMapping("/alterar/{id}")
     public ResponseEntity alterar(@PathVariable Integer id, @RequestBody UsuarioUpdateDTO dto){
         boolean valor = service.consultar(id);
